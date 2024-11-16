738c6b9227b6ceec8568959955f7143a30449b04
xxtsummer <2958975379@qq.com>
2024-11-16 19:58:52 +0800

    continue

--- 小熊猫c++/webclub  11.16  .c
+++ 小熊猫c++/webclub  11.16  .c
@@ -3,14 +3,30 @@
 
 
 int main(){
-	int a[5];
-	for(int i=1;i<=5;i++){
+	int c[5];
+	for(int i=0;i<5;i++){
 		//TODO
 	
-		printf("%d\n",a[i]);
+		printf("%d\n",c[i]);
 	}
-	
-	
+	int b=0;
+	for(int j=0;j<4;j++){
+		//TODO
+		for(int i=0;i<4-j;i++){
+			//TODO
+			
+			if (c[i]>c[i+1]){
+				b=c[i+1];
+			    c[i+1]=c[i];
+			    c[i]=b;
+			}
+		}	
+		
+	}
+	for(int i=0;i<5;i++){
+		printf("   %d\n",c[i]);
+//TODO
+	}
 	
 	
 	
